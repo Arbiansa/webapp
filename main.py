035540bab96893e21b06438fe86fe7d61ec9ea2e
Arbiansa <111883775+Arbiansa@users.noreply.github.com>
2022-12-15 02:26:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import streamlit as st
+st.session_state['answer'] = ''!
+
+st.write(st.session_state)
+
+realans = ['', 'abc', 'edf']
+
+if  st.session_state['answer'] in realans:
+    answerStat = "correct"
+elif st.session_state['answer'] not in realans:
+    answerStat = "incorrect"
+
+st.write(st.session_state)
+st.write(answerStat)
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
